7eb1d50b077ee15f6866381c2fd543da46426add
Soniku (KokiKitsune) <chompybird75@gmail.com>
2024-10-18 18:58:41 -0700

    Add files via upload

---
+++ hbas.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import zipfile
+import argparse
+import requests
+import time
+
+# Main Command Line functions
+parser = argparse.ArgumentParser(prog='HBAS-Downloader-CLI', description='Downloads Homebrew via CLI')
+parser.add_argument('-p', '--platform', type=str, required=True, help="The console you use")
+parser.add_argument('-d', '--download', type=str, required=True, help="Downloads the app (make sure to input it with .zip at the end)")
+
+subparser = parser.add_subparsers()
+# WiiuLoad Command Line functions
+wiiuLoad = subparser.add_parser('wiiuload')
+wiiuLoad.add_argument('-c', '--configure', action="store", help="Connect to the Wii U via ip")
+
+args = parser.parse_args()
+# Download functions
+def wii_u_download(url):
+    directory = input("Enter a directory name (This includes drive letters as well)")
+    wiiu_resp = requests.get(url)
+    wiiu_file_path = 'wiiu_downloaded.zip'
+    with open(wiiu_file_path, 'wb') as wiiu_file:
+        wiiu_file.write(wiiu_resp.content)
+        with zipfile.ZipFile(wiiu_file_path, 'r') as wiiu_ex:
+            wiiu_ex.extractall(directory)
+        print("Successfully downloaded your app!")
+        os.remove(wiiu_file_path)
+
+def nx_download(url):
+    directory = input("Enter a directory name (This includes drive letters as well)")
+    nx_resp = requests.get(url)
+    nx_file_path ='nx_downloaded.zip'
+    with open(nx_file_path, 'wb') as nx_file:
+        nx_file.write(nx_resp.content)
+        with zipfile.ZipFile(nx_file_path, 'r') as nx_ex:
+            nx_ex.extractall(directory)
+            print("Successfully downloaded your app!")
+# App Downloading configuration
+NX_valid = {"NX", "Nx", "Nintendo-Switch", "Nintendo-switch"}
+if args.platform == "Wii-U" or args.platform == "Wii-u":
+    print("Downloading your package for Wii U...")
+    wii_u_download('https://wiiu.cdn.fortheusers.org/zips/' + (args.download))
+elif args.platform == NX_valid:
+    print("Downloading your package for Nintendo Switch...")
+    nx_download('https://switch.cdn.fortheusers.org/zips/' + (args.download))
+else:
+    print("Argument invalid!  Aborting!")
+    time.sleep(1)
+    sys.exit()
